df5dd94d08dd4d9c70676aa9ceadc18c1170c838
Mykhailo Voitovych <m.voitovych21@gmail.com>
2022-06-02 22:39:04 +0300

    10lab Node relocated to BinaryTree class created logic for the tree

--- lab10/binary_tree.py
+++ lab10/binary_tree.py
@@ -1,83 +1,138 @@
-# from __future__ import *
+from typing import List
 
 
-class Student:
-    def __init__(self, name, surname, rating, group, birth_year) -> None:
-        self.name = name
-        self.surname = surname
-        self.rating = rating
-        self.group = group
-        self.birth_year = birth_year
+class BinaryTree:
+    class Node:
+        def __init__(self, key) -> None:
+            self.key = key
+            self.left_node = None
+            self.right_node = None
 
-    # def __eq__(self, other):
-    #     return self.rating == other.rating
+    def __init__(self) -> None:
+        self.root = None
 
-    def __lt__(self, other):
-        return self.rating < other.rating
+    def insert(self, key) -> None:
+        if self.root is None:
+            self.root = self.Node(key)
+            return
 
-    def __le__(self, other):
-        return self.rating <= other.rating
+        current_node = self.root
+        while current_node and current_node.key != key:
+            if current_node.key > key:
 
-    def __gt__(self, other):
-        return self.rating > other.rating
-
-    def __ge__(self, other):
-        return self.rating >= other.rating
-
-    def __str__(self):
-        return f"Student {self.name} {self.surname} with {self.rating} rating "
-
-
-class Node:
-    def __init__(self, key) -> None:
-        self.left_node = None
-        self.right_node = None
-        self.key = key
-
-    def insert_node(self, key) -> None:
-        if self.key is not None:
-            if self.key > key:
-                if self.left_node is None:
-                    self.left_node = Node(key)
+                if current_node.left_node is None:
+                    current_node.left_node = self.Node(key)
+                    return
                 else:
-                    self.left_node.insert_node(key)
+                    current_node = current_node.left_node
+            elif current_node.key < key:
 
-            elif self.key < key:
-                if self.right_node is None:
-                    self.right_node = Node(key)
+                if current_node.right_node is None:
+                    current_node.right_node = self.Node(key)
+                    return
                 else:
-                    self.right_node.insert_node(key)
+                    current_node = current_node.right_node
+
+    def find_by_key(self, key) -> bool:
+        if self.root is None:
+            return False
+
+        current_node = self.root
+        while current_node:
+            if current_node.key == key:
+                return True
+
+            if current_node.key > key:
+                current_node = current_node.left_node
+
+            elif current_node.key < key:
+                current_node = current_node.right_node
+
+        return False
+
+    def get_tree_in_list(self) -> List:
+        key_list = []
+
+        def __get_node_key(current_node) -> None:
+            if current_node is None:
+                return
+
+            __get_node_key(current_node.left_node)
+            __get_node_key(current_node.right_node)
+
+            key_list.append(current_node.key)
+
+        __get_node_key(self.root)
+        return key_list
+
+    def __recursive_print(self, current_node) -> None:
+        if current_node is None:
+            return
+        print(current_node.key)
+        self.__recursive_print(current_node.right_node)
+        self.__recursive_print(current_node.left_node)
+
+    def __recursive_print_nodes_larger_than(self, current_node, key) -> None:
+        if current_node is None:
+            return
+
+        self.__recursive_print_nodes_larger_than(current_node.right_node, key)
+        self.__recursive_print_nodes_larger_than(current_node.left_node, key)
+        if current_node.key > key:
+            print(current_node.key)
 
     def print_tree(self) -> None:
-        if self.left_node:
-            self.left_node.print_tree()
-        print(f"{self.key}")
+        current_node = self.root
+        self.__recursive_print(current_node)
 
-        if self.right_node:
-            self.right_node.print_tree()
+    def print_nodes_larger_than(self, key) -> None:
+        current_node = self.root
+        self.__recursive_print_nodes_larger_than(current_node, key)
 
-    def print_nodes_larger_than(self, argument) -> None:
-        if self.left_node:
-            self.left_node.print_nodes_larger_than(argument)
+    def delete_node(self, current_node, key) -> Node or None:
 
-        if self.key >= argument:
-            print(f"{self.key}")
+        if current_node is None:
+            return
 
-        if self.right_node:
-            self.right_node.print_nodes_larger_than(argument)
+        if key < current_node.key:
+            current_node.left_node = self.delete_node(current_node.left_node, key)
 
-    def search_for_key(self, key) -> int:
-        if self.key != key:
-            if self.key > key and self.left_node is not None:
-                if self.left_node.search_for_key(key):
-                    return 1
+        elif key > current_node.key:
+            current_node.right_node = self.delete_node(current_node.right_node, key)
 
-            elif self.key < key and self.right_node is not None:
-                if self.right_node.search_for_key(key):
-                    return 1
         else:
-            return 1
-        return 0
+            if current_node.left_node is None:
+                temp_node = current_node.right_node
+                return temp_node
 
-    def delete_node(self, key):
-        pass
+            elif current_node.right_node is None:
+                temp_node = current_node.left_node
+                return temp_node
+
+            temp_node = current_node.right_node
+
+            while temp_node.left_node:
+                temp_node = temp_node.left_node
+            current_node.key = temp_node.key
+            current_node.right_node = self.delete_node(current_node.right_node, temp_node.key)
+        return current_node
+
+    def __recursive_delete_node(self, current_node) -> None:
+        if current_node is None:
+            return
+        self.__recursive_delete_node(current_node.left_node)
+        self.__recursive_delete_node(current_node.right_node)
+        current_node.left_node = None
+        current_node.right_node = None
+
+    def delete_tree(self) -> None:
+        self.__recursive_delete_node(self.root)
+        self.root = None
+
+    def delete_all_by_group(self, group):
+        tree_list = self.get_tree_in_list()
+        new_tree = BinaryTree()
+        for key in tree_list:
+            if key.group != group:
+                new_tree.insert(key)
+        return new_tree
--- lab10/main.py
+++ lab10/main.py
@@ -1,20 +1,38 @@
 from binary_tree import *
+from student import *
 
 
 def main():
-    s1 = Student("Student1", "Surname1", 74, "ir11", 2021)
-    s2 = Student("Student2", "Surname2", 89, "ir12", 2021)
-    s3 = Student("Student3", "Surname3", 84, "ir13", 2021)
-    s4 = Student("Student4", "Surname4", 2, "ir14", 2021)
+    students_list = [Student("Oleg", "Bober", 74, "ir11", 2021),
+                     Student("Anton", "Tort", 89, "ir12", 2021),
+                     Student("Zenovij", "Keks", 84, "ir13", 2021),
+                     Student("Andrii", "Gurskiy", 24, "ir14", 2021),
+                     Student("Petro", "Sagaidachnyj", 51, "ir11", 2021),
+                     Student("Igor", "Neroba", 52, "ir11", 2021),
+                     Student("Svyat", "Krtyi", 53, "ir11", 2021)]
 
-    root = Node(s1)
-    root.insert_node(s2)
-    root.insert_node(s3)
-    root.insert_node(s4)
+    tree = BinaryTree()
+    for student in students_list:
+        tree.insert(student)
 
-    root.print_tree()
-    print("=========")
-    root.print_nodes_larger_than(s1)
+    print("======== all nodes ========")
+    tree.print_tree()
+
+    print("======== larger than 53 ========")
+    tree.print_nodes_larger_than(students_list[6])
+
+    print("========= after Student5 was deleted ========")
+    tree.delete_node(tree.root, students_list[4])
+    tree.print_tree()
+    tree.delete_all_by_group("ir11")
+
+    print("======== after ir11 was deleted  ========")
+    tree = tree.delete_all_by_group("ir11")
+    tree.print_tree()
+    tree.delete_tree()
+    print("======== ======== ======== ========")
+    if tree.root is None:
+        print("Horray! tree deleted successfully")
 
 
 if __name__ == '__main__':
